1123bac1ac6cdb99d28cca175f33d60db5140fed
YamalAli770 <yamalali770@gmail.com>
2023-08-26 13:55:19 +0500

    initial loading using framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -29,95 +29,70 @@ import CheckoutFailed from "./components/CheckoutFailed"
 import Order from "./components/Order"
 import AdminLogin from "./components/AdminLogin"
 import Dashboard from "./components/Dashboard"
+import Loading from "./components/Loading"
 
 function App() {
   const auth = useStore(state => state.auth);
   const user = useStore(state => state.user);
+  const [loading, setLoading] = useState(true);
 
   const [isAdminRoute, setIsAdminRoute] = useState(false);
 
   useEffect(() => {
     setIsAdminRoute(window.location.pathname.includes('admin'));
   }, []);
-  // const user = useStore((state) => state.user);
-  // const setUser = useStore((state) => state.setUser);
 
-  // const handleTokenRefresh = async () => {
-  //   try {
-  //     const response = await axios.post(`${API_URL}api/auth/refresh-token`, {}, {
-  //       withCredentials: true,
-  //       headers: {
-  //         'Authorization': 'Bearer ' + user.accessToken,
-  //       }
-  //     })
-  //     if(response.data) {
-  //       console.log(response.data);
-  //       setUser({
-  //         ...user,
-  //         accessToken: response.data.accessToken,
-  //       });
-  //       localStorage.setItem('user', JSON.stringify({
-  //         ...user,
-  //         accessToken: response.data.accessToken,
-  //       }));
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // useEffect(() => {
-    //   if(user && isAccessTokenCloseToExpiry(user.accessToken)) {
-      //     console.log("refreshing token");
-  //     handleTokenRefresh();
-  //   }
-  // }, [user]);
+  setTimeout(() => {
+    setLoading(false);
+  }, 5000);
   
   const [isCartOpen, setIsCartOpen] = useState(false);
   return (
-    <div className={isAdminRoute ? '' : 'bg-customBg text-customText'}>
-        <div className={isAdminRoute ? '' : 'max-w-custom m-auto'}>
-        <BrowserRouter>
-          {isCartOpen && <Cart setIsCartOpen={setIsCartOpen} />}
+    <>
+      {loading ? <Loading setLoading={setLoading} /> : <div className={isAdminRoute ? '' : 'bg-customBg text-customText'}>
+          <div className={isAdminRoute ? '' : 'max-w-custom m-auto'}>
+          <BrowserRouter>
+            {isCartOpen && <Cart setIsCartOpen={setIsCartOpen} />}
 
-         { !isAdminRoute && <Navbar setIsCartOpen={setIsCartOpen} />}
-          <Routes>
-            <Route path="/" element={
-              <>
-                <Hero />
-                <Feature />
-                <Gallery />
-                <Statistic />
-              </>
-            } />
-            <Route element={<ProtectedRoutes />}>
-              <Route path="/new" element={<ListProduct />} />
-            </Route>
-            <Route path="/register" element={auth ? <Navigate to="/" /> : <Register />} />
-            <Route path="/login" element={auth ? <Navigate to="/" /> : <Login /> } />
-            <Route path="/about" element={<About />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/shop/:id" element={<Product />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/success" element={<Success />} />
-            <Route path="/cancel" element={<Cancel />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/checkout/success" element={<CheckoutSuccess />} />
-            <Route path="/checkout/error" element={<CheckoutFailed />} />
-            <Route path="/order/:id" element={<Order />} />
+          { !isAdminRoute && <Navbar setIsCartOpen={setIsCartOpen} />}
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Hero />
+                  <Feature />
+                  <Gallery />
+                  <Statistic />
+                </>
+              } />
+              <Route element={<ProtectedRoutes />}>
+                <Route path="/new" element={<ListProduct />} />
+              </Route>
+              <Route path="/register" element={auth ? <Navigate to="/" /> : <Register />} />
+              <Route path="/login" element={auth ? <Navigate to="/" /> : <Login /> } />
+              <Route path="/about" element={<About />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/shop/:id" element={<Product />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/success" element={<Success />} />
+              <Route path="/cancel" element={<Cancel />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/checkout/success" element={<CheckoutSuccess />} />
+              <Route path="/checkout/error" element={<CheckoutFailed />} />
+              <Route path="/order/:id" element={<Order />} />
 
-            <Route path="*" element={<Error />} />
-            
-            {/* Admin Routes */}
-            <Route path="/admin" element={ auth && user.isAdmin === 'true' ? <Navigate to="/admin/dashboard" /> : auth ? <Navigate to="/" replace={true} /> : <AdminLogin />} />
-            <Route path="/admin/dashboard" element={ auth && user.isAdmin ? <Dashboard /> : <Navigate to="/" /> } />
+              <Route path="*" element={<Error />} />
+              
+              {/* Admin Routes */}
+              <Route path="/admin" element={ auth && user.isAdmin === 'true' ? <Navigate to="/admin/dashboard" /> : auth ? <Navigate to="/" replace={true} /> : <AdminLogin />} />
+              <Route path="/admin/dashboard" element={ auth && user.isAdmin ? <Dashboard /> : <Navigate to="/" /> } />
 
-          </Routes>
-          { !isAdminRoute &&  <Footer />}
-        </BrowserRouter>
-        </div>!
-      </div>
+            </Routes>
+            { !isAdminRoute &&  <Footer />}
+          </BrowserRouter>
+          </div>
+        </div>}
+      </>
   )
 }
 
